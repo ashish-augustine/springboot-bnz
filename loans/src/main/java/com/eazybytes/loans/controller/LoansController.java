e824c47bcd4fce9f9b30cb309e41a5c49c6e5b63
Ashish Augustine <ashish.augustine123@gmail.com>
2025-05-20 19:18:11 +0200

    Update LoansController.java

--- loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -3,7 +3,7 @@ package com.eazybytes.loans.controller;
 import com.eazybytes.common.dto.ErrorResponseDto;
 import com.eazybytes.loans.constants.LoansConstants;
 import com.eazybytes.loans.dto.LoansContactInfoDto;
-import com.eazybytes.loans.dto.LoansDto;
+import com.eazybytes.loans.dto.LoansDto; 
 import com.eazybytes.loans.dto.ResponseDto;
 import com.eazybytes.loans.service.ILoansService;
 import io.swagger.v3.oas.annotations.Operation;
