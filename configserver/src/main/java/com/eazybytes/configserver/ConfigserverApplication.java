e6addd2fa0c443fc731eeebe28429d7b7f2965d1
Ashish Augustine <ashish.augustine123@gmail.com>
2025-05-20 19:17:33 +0200

    EnableConfigServer

--- configserver/src/main/java/com/eazybytes/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/eazybytes/configserver/ConfigserverApplication.java
@@ -1,6 +1,6 @@
 package com.eazybytes.configserver;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
