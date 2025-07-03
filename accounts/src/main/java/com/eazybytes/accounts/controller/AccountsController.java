a0da31d79b03c653af818f71b39ccdfd5932ab02
Ashish Augustine <ashish.augustine123@gmail.com>
2025-07-03 15:32:52 +0200

    Update and fetch account details

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -27,9 +27,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * @author Eazy Bytes
- */
+
+
 
 @Tag(
         name = "CRUD REST APIs for Accounts in EazyBank",
